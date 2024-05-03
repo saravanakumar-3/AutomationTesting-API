70530274319288c1ddd4c60223b63d56c19ed5c3
Saravanakumar <saravanakumar.2995@gmail.com>
2024-05-03 20:30:31 +0530

    initial commit

---
+++ src/main/java/com/test/automation/api/AutomationTestingApiApplication.java
@@ -0,0 +1,13 @@
+package com.test.automation.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomationTestingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomationTestingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/automation/api/AutomationTestingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.automation.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomationTestingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
